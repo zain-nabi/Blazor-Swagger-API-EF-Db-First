b7798eda3b4c8e307ba0457993df443223853818
Zain Nabi <zainn@tritonexpres.co.za>
2022-02-24 19:03:55 +0200

    Initial Commit
    Initial Commit

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Blazor_API.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_API.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
